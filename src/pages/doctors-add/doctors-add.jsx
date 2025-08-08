9dbef6faa343e54c2cb8bcb0f549b640cf18dd84
Jhonathan de Lima <167260048+jhonathancodes@users.noreply.github.com>
2025-08-08 09:52:47 -0300

    Update doctors-add.jsx

--- src/pages/doctors-add/doctors-add.jsx
+++ src/pages/doctors-add/doctors-add.jsx
@@ -1,17 +1,17 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import api from "../../constants/api.js"; // Importando a instância do Axios
+import api from "../../constants/api.js"; 
 import Navbar from "../../components/navbar/navbar.jsx";
 
 function DoctorsAdd() {
   const navigate = useNavigate();
-  const [name, setName] = useState(""); // Nome do Setor
-  const [specialization, setSpecialization] = useState(""); // Serviços (não é necessário na tabela doctors)
-  const [icon, setIcon] = useState(""); // Ícone do Setor
+  const [name, setName] = useState(""); 
+  const [specialization, setSpecialization] = useState(""); 
+  const [icon, setIcon] = useState(""); 
 
-  // Função para salvar o setor (doctor) no banco de dados
+  
   async function saveDoctor() {
-    // Payload para salvar na tabela 'doctors' com name e icon
+   
     const jsonDoctor = {
       name: name,
       icon: icon,
@@ -19,32 +19,28 @@ function DoctorsAdd() {
 
     try {
       const responseDoctor = await api.post("/doctors", jsonDoctor);
-      // Chama a rota de inserir na tabela 'doctors'
+   
 
       if (responseDoctor.data) {
         const doctorId = responseDoctor.data.id_doctor;
-        // Obtemos o ID do médico criado
-
-        // Agora, vamos inserir o serviço
+       
         const jsonService = {
           description: specialization,
         };
         const responseService = await api.post("/services", jsonService);
-        // Chama a rota de inserir na tabela 'services'
+        
 
         if (responseService.data) {
           const serviceId = responseService.data.id_service;
-          // Obtemos o ID do serviço criado
-
-          // Após inserir os dados do médico e do serviço, vamos associá-los.
+          
           const jsonDoctorService = {
             id_doctor: doctorId,
             id_service: serviceId,
           };
-          // Associe o médico ao serviço
+        
           await api.post("/doctors/services", jsonDoctorService);
 
-          // Se tudo der certo, redireciona para a lista de médicos
+         
           navigate("/doctors");
         }
       }
@@ -53,9 +49,9 @@ function DoctorsAdd() {
     }
   }
 
-  // Função de cancelar (voltar para a lista de médicos)
+
   function handleCancel() {
-    navigate("/doctors"); // Redireciona para a lista de médicos
+    navigate("/doctors"); 
   }
 
   return (
@@ -76,7 +72,7 @@ function DoctorsAdd() {
               className="form-control"
               value={name}
               onChange={(e) => setName(e.target.value)}
-              // Atualiza o estado do nome
+              
             />
           </div>
           <div className="col-12 mt-4">
@@ -89,7 +85,7 @@ function DoctorsAdd() {
               className="form-control"
               value={specialization}
               onChange={(e) => setSpecialization(e.target.value)}
-              // Atualiza o estado da especialização
+            
             />
           </div>
           <div className="col-12 mt-4">
@@ -102,14 +98,14 @@ function DoctorsAdd() {
               className="form-control"
               value={icon}
               onChange={(e) => setIcon(e.target.value)}
-              // Atualiza o estado do ícone
+            
             />
           </div>
           <div className="col-12 mt-4">
             <div className="d-flex justify-content-between">
               <button
                 onClick={saveDoctor}
-                // Chama a função de salvar
+               
                 className="btn btn-primary"
                 type="button"
               >
@@ -117,7 +113,7 @@ function DoctorsAdd() {
               </button>
               <button
                 onClick={handleCancel}
-                // Chama a função de cancelar
+            
                 className="btn btn-outline-secondary"
                 type="button"
               >
