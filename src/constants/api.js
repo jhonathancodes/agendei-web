6a5f4165b8059df03cf736b49288285ad5027075
Jhonathan de Lima <167260048+jhonathancodes@users.noreply.github.com>
2025-08-08 09:50:44 -0300

    Update api.js

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,18 +1,18 @@
 import axios from "axios";
 
-// Criando uma instância do Axios para configurar as requisições com o token
+
 const api = axios.create({
-    baseURL: "http://localhost:3001", // URL base da API
+    baseURL: "http://localhost:3001",
 });
 
-// Interceptador para adicionar o token no cabeçalho das requisições
+
 api.interceptors.request.use(
     (config) => {
-        // Obtendo o token armazenado no localStorage
+      
         const token = localStorage.getItem("authToken");
 
         if (token) {
-            // Adiciona o token no cabeçalho Authorization
+            
             config.headers["Authorization"] = `Bearer ${token}`;
         }
 
